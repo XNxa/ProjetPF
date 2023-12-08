cb1095140755d4ec61990b264144f4619af08dfc
XNxa <xaviernaxara@yahoo.fr>
2023-12-08 14:41:04 +0100

    New First Commit

--- bin/newtonoid.ml
+++ bin/newtonoid.ml
@@ -1,58 +1,51 @@
 (* ouvre la bibliotheque de modules definis dans lib/ *)
 open Libnewtonoid
-
-(* exemple d'ouverture d'un tel module de la bibliotheque : *)
 open Iterator
+
+(* exemple d'ouvertue d'un tel module de la bibliotheque : *)
+open Game
+
+module Init = struct
+  let dt = 1000. /. 60. (* 60 Hz *)
+end
 
 module Box = struct
   let marge = 10.
-  let infx = 0.
-  let infy = 0.
-  let supx = 800.
-  let supy = 600.
-
-  let graphic_format =
-    Format.sprintf
-      " %dx%d+50+50"
-      (int_of_float ((2. *. marge) +. supx -. infx))
-      (int_of_float ((2. *. marge) +. supy -. infy))
+  let infx = 10.
+  let infy = 10.
+  let supx = 790.
+  let supy = 590.
 end
 
-module Init = struct
-  (* 16ms : 60 Hz *)
-  (* À UTILISER AUSSI DANS LE CALCUL D'INTÉGRALES *)
-  let dt = 0.016
-end
+let graphic_format =
+  Format.sprintf
+    " %dx%d+50+50"
+    (int_of_float ((2. *. Box.marge) +. Box.supx -. Box.infx))
+    (int_of_float ((2. *. Box.marge) +. Box.supy -. Box.infy))
 
-let run_game etats_jeu =
-  let ref_etats_jeu = ref etats_jeu in
-  let ref_hdlr_alarm = ref Sys.(Signal_handle (fun _ -> ())) in
-  let ref_hdlr_interruption = ref Sys.(Signal_handle (fun _ -> ())) in
-  let handler_alrm _ =
-    match Flux.uncons !ref_etats_jeu with
-    | None ->
-      Sys.(set_signal sigalrm !ref_hdlr_alarm);
-      Sys.(set_signal sigint !ref_hdlr_interruption)
-    | Some (etat, etats_jeu') ->
+let draw_state etat = failwith "A DEFINIR"
+
+(* extrait le score courant d'un etat : *)
+let score etat : int = failwith "A DEFINIR"
+
+let draw flux_etat =
+  let rec loop flux_etat last_score =
+    match Flux.(uncons flux_etat) with
+    | None -> last_score
+    | Some (etat, flux_etat') ->
       Graphics.clear_graph ();
-      (* PLACER LE CODE DE DESSIN (FONCTION D'etat) ICI : *)
-
-      (* FIN CODE DESSIN *)
+      (* DESSIN ETAT *)
+      draw_state etat;
+      (* FIN DESSIN ETAT *)
       Graphics.synchronize ();
-      ref_etats_jeu := etats_jeu'
+      Unix.sleepf Init.dt;
+      loop flux_etat' (last_score + score etat)
+    | _ -> assert false
   in
-  let handler_int _ = ref_etats_jeu := Flux.vide in
-  Sys.(ref_hdlr_alarm := signal sigalrm (Signal_handle handler_alrm));
-  Sys.(ref_hdlr_interruption := signal sigint (Signal_handle handler_int));
-  ignore Unix.(setitimer ITIMER_REAL { it_interval = Init.dt; it_value = Init.dt });
-  ignore (read_line ())
-
-let etats_jeu = Tick (lazy (failwith "À DÉFINIR"))
-
-let () =
-  Graphics.open_graph Box.graphic_format;
+  Graphics.open_graph graphic_format;
   Graphics.auto_synchronize false;
-  (* À DÉCOMMENTER UNE FOIS LE FLUX D'ÉTATS DÉFINI :*)
-  (* run_game etats_jeu;  *)
-  ignore (read_line ());
+  let score = loop flux_etat 0 in
+  Format.printf "Score final : %d@\n" score;
   Graphics.close_graph ()
+
+let () = game_hello ()
